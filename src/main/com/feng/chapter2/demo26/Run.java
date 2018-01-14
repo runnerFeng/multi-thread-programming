dc8b83ab5d3ee3b2f10fd2abcce814d29c5c3648
jinx <631819061@qq.com>
2018-01-14 14:32:55 +0800

    1.使用多线程解决同步死循环问题，但应该注意执行Thread类的run方法也需要消耗的时间，此时主线程可能会修改从线程持有的锁对象的变量的值,等执行完Thread的run方法开始执行从线程的方法时可能造成结果异常。

--- src/main/com/feng/chapter2/demo26/Run.java
+++ src/main/com/feng/chapter2/demo26/Run.java
@@ -3,7 +3,7 @@ package com.feng.chapter2.demo26;
 /**
  * @author jinx
  * @date 2018/1/12 22:29
- * Desc:同步死循环问题
+ * Desc:同步死循环问题，注意此时两个类中获取的线程都是main线程
  */
 public class Run {
     public static void main(String[] args) {
--- src/main/com/feng/chapter2/demo27/PrintString.java
+++ src/main/com/feng/chapter2/demo27/PrintString.java
@@ -5,14 +5,14 @@ package com.feng.chapter2.demo27;
  * @date 2018/1/12 22:27
  * Desc:
  */
-public class PrintString implements Runnable{
+public class PrintString implements Runnable {
     private boolean isContinuePrintString = true;
 
-    public void printStringMethod(){
-        while (isContinuePrintString == true){
-            System.out.println("run printStringMethod threadName:"+Thread.currentThread().getName());
+     public void printStringMethod() {
+        while (isContinuePrintString == true) {
+            System.out.println("run printStringMethod threadName:" + Thread.currentThread().getName());
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
