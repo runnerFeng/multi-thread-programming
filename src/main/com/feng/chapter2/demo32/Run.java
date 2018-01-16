4ac43be6fec929c7995416a809cffebc8457fb7b
jinx <631819061@qq.com>
2018-01-16 20:48:50 +0800

    1.注释修正

--- src/main/com/feng/chapter2/demo32/Run.java
+++ src/main/com/feng/chapter2/demo32/Run.java
@@ -3,7 +3,7 @@ package com.feng.chapter2.demo32;
 /**
  * @author jinx
  * @date 2018/1/14 22:56
- * Desc:该测试中虽然addAndGet方法内不是原子性的，但是两次调用去不能保证同步，所以输出结果有误，解决这样的问题必须使用同步。
+ * Desc:该测试中虽然addAndGet方法内是原子性的，但是两次调用却不能保证同步，所以输出结果有误，解决这样的问题必须使用同步。
  */
 public class Run {
     public static void main(String[] args) throws InterruptedException {
