72d2cdcb23bbf08626ca9512dc0f86f66daf4425
runnerFeng <631819061@qq.com>
2018-03-27 22:18:39 +0800

    1.修正错误注释

--- src/main/com/feng/chapter4/demo22/Run.java
+++ src/main/com/feng/chapter4/demo22/Run.java
@@ -3,7 +3,7 @@ package com.feng.chapter4.demo22;
 /**
  * @author jinx
  * @date 2018/03/27 21:12
- * Desc:boolean tryLock(long timeout, TimeUnit unit)如果在给定时间内没被另一个线程保持，切当前线程未被中断则获取该线程
+ * Desc:boolean tryLock(long timeout, TimeUnit unit)如果在给定时间内没被另一个线程保持，且当前线程未被中断则获取该线程
  *      lock级别
  */
 public class Run {
