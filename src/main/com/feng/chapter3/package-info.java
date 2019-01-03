1e01f43d91879a98716aa79b1ca80ef9fb189c0d
liufeng <631819061@qq.com>
2019-01-03 11:21:57 +0800

    1.添加包说明

---
+++ src/main/com/feng/chapter3/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author jinx
+ * @date 2019/1/3 11:20
+ * Desc:线程间通信
+ */
+package com.feng.chapter3;
---
+++ src/main/com/feng/chapter4/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author jinx
+ * @date 2019/1/3 11:21
+ * Desc:lock的使用
+ */
+package com.feng.chapter4;
